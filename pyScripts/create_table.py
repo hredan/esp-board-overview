ca00119ffa8db646b8cab7f88ce81a0ab7634025
hredan <hredan@sleepuino.de>
2025-11-08 07:48:29 +0000

    fix pylint error (pylint 4.0.2)

--- pyScripts/create_table.py
+++ pyScripts/create_table.py
@@ -23,8 +23,8 @@ if __name__ == "__main__":
     for core_info in core_info_list:
         core_name = core_info["core_name"]
         core_version = core_info["latest_version"]
-        CORE_DATA_PATH = f"./esp_data/{core_name}-{core_version}"
-        cd = CoreData(core_info["core_name"], core_info["installed_version"], CORE_DATA_PATH)
+        core_data_path = f"./esp_data/{core_name}-{core_version}"
+        cd = CoreData(core_info["core_name"], core_info["installed_version"], core_data_path)
         print(f"### core: {core_name} ###")
         print(f"number of boards: {len(cd.boards)}")
         print(f"number of boards without led: {cd.num_of_boards_without_led}")
