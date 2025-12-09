a6a7f4e1c135af6d8f87f6e8b31371f17aa575bf
hredan <hredan@sleepuino.de>
2025-12-09 14:34:24 +0000

    sanity check: add logging, do changes only in case it is wrong

--- pyScripts/partition_sanity_check.py
+++ pyScripts/partition_sanity_check.py
@@ -3,10 +3,13 @@ import json
 
 ESP32_JSON_PATH = "./esp_data/esp32_partitions.json"
 
+print("####### Starting sanity check #######")
 with open(ESP32_JSON_PATH, 'r', encoding='utf-8') as file:
     esp32_partitions = json.load(file)
 
 # check aslcanx2
-esp32_partitions["aslcanx2"]["schemes"]["defaultffat"]["build"] = "default_ffat_8MB"
+if esp32_partitions["aslcanx2"]["schemes"]["defaultffat"]["build"] == "default_8MB_ffat":
+    print("Modifying: aslcanx2 defaultffat build from default_8MB_ffat to default_ffat_8MB")
+    esp32_partitions["aslcanx2"]["schemes"]["defaultffat"]["build"] = "default_ffat_8MB"
 with open(ESP32_JSON_PATH, 'w', encoding='utf-8') as file:
     json.dump(esp32_partitions, file, ensure_ascii=False, indent=4)
