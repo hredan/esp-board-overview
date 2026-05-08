d1dc9a8be131e8ebe1e04519485a5cb3b25db74f
hredan <hredan@sleepuino.de>
2026-05-08 11:29:04 +0000

    update page description

--- web-app/src/app/esp-core-overview/esp-core-overview.component.ts
+++ web-app/src/app/esp-core-overview/esp-core-overview.component.ts
@@ -13,7 +13,7 @@ export class EspCoreOverviewComponent implements OnInit {
   coreList: Core[] = (coreList_input as Core[]);
   displayedColumns: string[] = ['core_name', 'core', 'installed_version', 'link'];
   dataSource = this.coreList;
-
+  esp32Version: string = this.coreList.find(core => core.core_name === 'esp32')?.installed_version || 'N/A';
   ngOnInit() {
     if (this.coreList.length == 2 && this.coreList[0].core_name == "esp8266" && this.coreList[1].core_name == "esp32") {
       this.coreList[0].link = "/esp8266";
